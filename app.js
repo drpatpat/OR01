18877602f70010c06739d67594fe69f43f0c2719
drpatpat <drpatpat@live.com>
2015-02-27 01:49:10 +0000

    Initial commit.

--- app.js
+++ app.js
@@ -1,60 +1,73 @@
+/**
+* Created with OR01.
+* User: drpatpat
+* Date: 2015-02-26
+* Time: 11:21 AM
+* To change this template use Tools | Templates.
+*/
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/', function(req, res) {
+    res.render('home');
 });
 
-// error handlers
+var fortunes = [
+    "The only way to do great work is to love what you do.",
+    "If you know you are going to fail, then fail gloriously.",
+    "You may have to fight a battle more than once to win it.",
+    "If you love life, don't waste time, for time is what life is made up of.",
+    "Happiness is not something readymade. It comes from your own actions.",
+    "Try not to become a person of success, but rather try to become a person of value.",
+    "You become what you believe.",
+    "It's never to late - never to late to change your life, never to late to be happy.",
+    "Life is what happens to you while you’re busy making other plans.",
+    "Find out who you are and be that person. That's what your soul was put on this Earth to be. Find that truth, live that truth and everything else will come.",
+    "Success is walking from failure to failure with no loss of enthusiasm.",
+    "Nothing is impossible, the word itself says, 'I’m possible!'",
+    "The most difficult thing is the decision to act, the rest is merely tenacity.",
+    "Life is very interesting...in the end, some of your greatest pains, become your greatest strengths.",
+    "The minute that you’re not learning I believe you’re dead.",
+    "We all keep dreaming, and luckily, dreams really do come true.",
+    "I believe there’s an inner power that makes winners or losers. And the winners are the ones who really listen to the truth of their hearts.",
+    "I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.'!",
+    "There are no regrets in life. Just lessons.",
+    "If you accept the expectations of others, especially negative ones, then you never will change the outcome.",
+    "Focusing your life solely on making a buck shows a certain poverty of ambition. It asks too little of yourself. Because it’s only when you hitch your wagon to something larger than yourself that you realize your true potential.",
+    "When you have a dream, you've got to grab it and never let go.",
+    "Learning how to be still, to really be still and let life happen - that stillness becomes a radiance.",
+    "It always seems impossible until it is done.",
+    "Winning isn’t everything, but wanting to win is.",
+];
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.get('/about', function(req, res) {
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about',{fortune:randomFortune});
 });
 
+// 404 catch-all handler (middleware)
+app.use(function(req,res) {
+    res.status(404);
+    res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err,req,res,next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
+});
 
 module.exports = app;
